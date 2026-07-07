a7056d6ff5b5846fe14e0e58c1a778a30627bda1
SANGMIN PARK <71458064+pp8817@users.noreply.github.com>
2026-07-08 01:45:19 +0900

    docs: Swagger bearer 인증 스키마 추가

--- src/main/java/com/landit/landitbe/LanditBeApplication.java
+++ src/main/java/com/landit/landitbe/LanditBeApplication.java
@@ -1,6 +1,8 @@
 // Landit 백엔드 애플리케이션의 진입점을 정의한다.
 package com.landit.landitbe;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import jakarta.annotation.PostConstruct;
 import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
@@ -8,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@SecurityScheme(name = "bearerAuth", type = SecuritySchemeType.HTTP, scheme = "bearer", bearerFormat = "JWT")
 @EnableScheduling
 @ConfigurationPropertiesScan
 @SpringBootApplication
