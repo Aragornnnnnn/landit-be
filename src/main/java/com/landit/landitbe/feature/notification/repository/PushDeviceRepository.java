3aba3d0de682417f7bfcada09f04aed6f23a9f19
SANGMIN PARK <71458064+pp8817@users.noreply.github.com>
2026-07-26 16:12:47 +0900

    feat: PUSH_SEND 메시지로 설치별 발송을 분리한다

--- src/main/java/com/landit/landitbe/feature/notification/repository/PushDeviceRepository.java
+++ src/main/java/com/landit/landitbe/feature/notification/repository/PushDeviceRepository.java
@@ -85,6 +85,7 @@ public interface PushDeviceRepository extends JpaRepository<PushDevice, Long> {
       where device.id = :pushDeviceId
       """)
   Optional<PushDevice> findByIdForUpdate(@Param("pushDeviceId") Long pushDeviceId);
+
   /**
    * 한 사용자의 발송 가능한 Push Device를 조회한다.
    *
