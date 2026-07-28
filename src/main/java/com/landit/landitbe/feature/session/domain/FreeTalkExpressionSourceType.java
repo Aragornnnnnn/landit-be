468abdc68166521e23ea93f97d34b682db8e1996
SANGMIN PARK <71458064+pp8817@users.noreply.github.com>
2026-07-28 14:54:24 +0900

    feat: 프리톡 맞춤 표현 생성 상태 추가

---
+++ src/main/java/com/landit/landitbe/feature/session/domain/FreeTalkExpressionSourceType.java
@@ -0,0 +1,9 @@
+// 프리톡 표현이 기존 Writing 표현인지 신규 생성 표현인지 정의한다.
+
+package com.landit.landitbe.feature.session.domain;
+
+/** 프리톡 표현이 기존 Writing 표현인지 신규 생성 표현인지 정의한다. */
+public enum FreeTalkExpressionSourceType {
+  EXISTING,
+  NEW
+}
