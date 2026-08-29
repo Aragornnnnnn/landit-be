1912fcca3cf70a81d316fff65cecc5126d97f475
SANGMIN PARK <71458064+pp8817@users.noreply.github.com>
2026-08-29 22:11:36 +0900

    feat: 사용자별 알림 계산 상태 스냅샷을 저장한다

--- src/main/java/com/landit/landitbe/feature/notification/domain/NotificationType.java
+++ src/main/java/com/landit/landitbe/feature/notification/domain/NotificationType.java
@@ -4,6 +4,9 @@ package com.landit.landitbe.feature.notification.domain;
 
 /** 백엔드가 발송하는 푸시 알림 유형을 정의한다. */
 public enum NotificationType {
+  CONTINUE_SCENARIO,
+  CONTINUE_EXPRESSION,
+  REVIEW_LEARNING,
   REVIEW_REMINDER,
   TEST_NOTIFICATION
 }
